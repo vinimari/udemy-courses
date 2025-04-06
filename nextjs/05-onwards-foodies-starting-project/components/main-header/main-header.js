bad6c9b9b856d3678c19c6f3d7d6548cf8c9aa3e
Marcio Vinicius de Carvalho Marinho <mmarinho@ciandt.com>
2025-04-06 19:09:09 -0300

    nextjs: feat: add dynamic active links route

--- nextjs/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ nextjs/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -2,6 +2,7 @@ import logoImg from "@/assets/logo.png";
 import Link from "next/link";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import NavLink from "../nav-link";
 
 export default function MainHeader() {
   return (
@@ -14,12 +15,12 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <NavLink href={"/meals"}>Browse Meals</NavLink>
           </li>
         </ul>
         <ul>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <NavLink href={"/community"}>Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
