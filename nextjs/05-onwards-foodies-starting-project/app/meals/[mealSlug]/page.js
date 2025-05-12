10a90b6f4d7fd10bd5cc20aa084738c8b2073bb6
vinimari <viniciusmarinho44@hotmail.com>
2025-05-11 21:58:17 -0300

    feat: add metadada

--- nextjs/05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ nextjs/05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealsDetailPage({ params }) {
   const meal = getMeal(params.mealSlug);
   if (!meal) {
