6a8d407b1c8543f9dff6caf0fcfb646faf025528
vinimari <viniciusmarinho44@hotmail.com>
2025-05-17 18:10:24 -0300

    feat: added dummy news

--- nextjs/01-starting-project/app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function DynamicNews({ params }) {
-  const newsId = params.id;
-  return (
-    <>
-      <h1>News Detail Page</h1>
-      <p>My news ID is: {newsId}</p>
-    </>
-  );
-}
--- nextjs/01-starting-project/app/news/page.js
+++ nextjs/01-starting-project/app/news/page.js
@@ -1,19 +1,19 @@
+import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/test1">Test 1</Link>
-        </li>
-        <li>
-          <Link href="/news/test2">Test 2</Link>
-        </li>
-        <li>
-          <Link href="/news/test3">Test 3</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img src={`images/news/${newsItem.image}`} alt={newsItem.title} />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
